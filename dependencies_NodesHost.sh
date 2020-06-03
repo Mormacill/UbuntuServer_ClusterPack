c5b103e1fb8a8ac86d8f35813523b20c66047fcb
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-06-03 12:22:04 +0200

    Update dependencies_NodesHost.sh

--- dependencies_NodesHost.sh
+++ dependencies_NodesHost.sh
@@ -5,4 +5,4 @@ sudo apt install -y  apt-transport-https ca-certificates curl gnupg-agent softwa
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
