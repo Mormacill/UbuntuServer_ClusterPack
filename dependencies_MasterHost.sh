3450144fd5d9a660df9ecfc0dfbe34965723c31d
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-06-03 11:56:36 +0200

    Update dependencies_MasterHost.sh

--- dependencies_MasterHost.sh
+++ dependencies_MasterHost.sh
@@ -8,7 +8,7 @@ sudo echo '/home/mpiuser *(rw,sync,no_subtree_check)' >> /etc/exports
 
 #DOCKER
 sudo apt install -y  apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install install docker-ce docker-ce-cli containerd.io
+sudo apt install -y install docker-ce docker-ce-cli containerd.io
