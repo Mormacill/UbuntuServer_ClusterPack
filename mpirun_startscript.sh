dbc044036448a945ed55a86bd5d0fbfc27b9cde7
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-06-08 15:25:37 +0200

    Create mpirun_startscript.sh

---
+++ mpirun_startscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Name des Hostfiles"
+read HOSTFILE
+echo ""
+echo "Anzahl der Prozesse"
+read NUMBEROFCORES
+echo ""
+echo "Welcher Solver soll verwendet werden? (e.g simpleFoam, pimpleFoam...)"
+read EXECUTABLE
+echo ""
+
+mpirun --hostfile $HOSTFILE --mca btl_tcp_if_include 10.0.1.0/24 -np $NUMBEROFCORES /opt/openfoam7/platforms/linux64GccDPInt32Opt/bin/$EXECUTABLE -parallel
