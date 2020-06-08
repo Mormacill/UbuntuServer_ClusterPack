c218c98d4501f05b856dd6a0a7eb997672749586
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-06-08 22:10:01 +0200

    Update mpirun_startscript.sh

--- mpirun_startscript.sh
+++ mpirun_startscript.sh
@@ -9,4 +9,4 @@ echo "Welcher Solver soll verwendet werden? (e.g simpleFoam, pimpleFoam...)"
 read EXECUTABLE
 echo ""
 touch log
-tilix -s 'log output of current simulation' -e 'watch -n 0.2 tail -n 25 log' & mpirun --hostfile $HOSTFILE --mca btl_tcp_if_include 10.0.1.0/24 -np $NUMBEROFCORES /opt/openfoam7/platforms/linux64GccDPInt32Opt/bin/$EXECUTABLE -parallel > log 
+tilix --geometry=125x30 --window-style=disable-csd-hide-toolbar -t 'log output of current simulation' -e 'watch -n 0.2 tail -n 25 log' & mpirun --hostfile $HOSTFILE --mca btl_tcp_if_include 10.0.1.0/24 -np $NUMBEROFCORES /opt/openfoam7/platforms/linux64GccDPInt32Opt/bin/$EXECUTABLE -parallel > log 
